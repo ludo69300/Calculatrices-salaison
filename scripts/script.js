2e178824f79ad317cb691c23b20fe1a674a2a464
Ludovic Cellard <ludovic.cellard@free.fr>
2026-01-29 15:14:03 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,5 @@
-function calculFrRotation(vc, diam) {
-	return parseInt((1000*vc)/(Math.PI*diam));
-};
-function calculAvance(n,fz,Z){
-	return parseInt(n*fz*Z);
-};
-function calculAvanceZ(vc){
-	document.getElementById("vitesseMaxAvanceZ").value = vc/2;
-};
 function claculSalaison() {
-	console.log("Hellos");
+	//console.log("Hellos");
 	let inputsalaisonSelect = document.getElementById("salaisonSelect");
 	let inputsalaisonPoids = document.getElementById("salaisonPoids");
 
@@ -24,7 +15,6 @@ function claculSalaison() {
 		calculssalaisonVide()
 	});
 	function calculssalaisonVide() {
-		console.log("Hello World");
 		document.getElementById("salaisonSel").value=indexsalaisonSelect*indexsalaisonPoids+' gr';
 		document.getElementById("salaisonSucre").value=indexsalaisonSelect*indexsalaisonPoids/2+' gr';
 		document.getElementById("salaisonPoivre").value=indexsalaisonPoids*0.001+' gr';
@@ -55,14 +45,50 @@ function claculSalaison() {
 	let indexfoiePoids = parseInt(inputfoiePoids.value);
 	inputfoiePoids.addEventListener("change", () => {
 		indexfoiePoids = parseInt(inputfoiePoids.value);
-		document.getElementById("foieSel").value=indexsaumureeau*60+' gr';
-		document.getElementById("saumureSucre").value=indexsaumureeau*30+' gr';
-		document.getElementById("saumurePoivre").value=indexsaumureeau*5+' gr';
-		document.getElementById("saumureBaie").value=indexsaumureeau*10+' gr';
-		document.getElementById("saumureSauge").value=indexsaumureeau*2+' gr';
-		document.getElementById("saumureThym").value=indexsaumureeau+' branche(s)';
-		document.getElementById("saumureClou").value=indexsaumureeau+' clou(s)';
-		document.getElementById("saumureLaurier").value=indexsaumureeau*2+' Feuilles';
+		document.getElementById("foieSel").value=indexfoiePoids*0.012+' gr';
+		document.getElementById("foieSucre").value=indexfoiePoids*0.006+' gr';
+		document.getElementById("foiePoivre").value=indexfoiePoids*0.002+' gr';
+		document.getElementById("foieEpices").value=indexfoiePoids*0.001+' gr';
+		document.getElementById("foieLiquoreux").value=indexfoiePoids*30+' gr';
+	});
+	let inputsaumonPoids = document.getElementById("saumonPoids");
+	let indexsaumonPoids = parseInt(inputsaumonPoids.value);
+	inputsaumonPoids.addEventListener("change", () => {
+		indexsaumonPoids = parseInt(inputsaumonPoids.value);
+		document.getElementById("saumonSel").value=Math.round(indexsaumonPoids*34/10)/100+' gr';
+		document.getElementById("saumonSucre").value=Math.round(indexsaumonPoids*17/10)/100+' gr';
+		document.getElementById("saumonBaie").value=Math.round(indexsaumonPoids/10)/100+' gr';
+	});
+	let inputpizza = document.getElementById("pizzaPoids");
+	let indexpizza = parseInt(inputpizza.value);
+	inputpizza.addEventListener("change", () => {
+		indexpizza = parseInt(inputpizza.value);
+		document.getElementById("pizzaFarine").value=Math.round(indexpizza*614/10)/100+' gr';
+		document.getElementById("pizzaEau").value=Math.round(indexpizza*368/10)/100+' gr';
+		document.getElementById("pizzaSel").value=Math.round(indexpizza*18/10)/100+' gr';
+		document.getElementById("pizzaLevure").value=Math.round(indexpizza*198/1000)/100+' gr';
+	});
+	let inputbuns = document.getElementById("bunsNb");
+	let indexbuns = parseInt(inputbuns.value);
+	inputbuns.addEventListener("change", () => {
+		indexbuns = parseInt(inputbuns.value);
+		let poids=indexbuns*100;
+		document.getElementById("bunsPoids").value=poids+' gr';
+		let farine=Math.round(indexbuns*52356/10)/100;
+		document.getElementById("bunsFarine").value=farine+' gr';
+		let huile=Math.round(indexbuns*1047/10)/100;
+		document.getElementById("bunsHuile").value=huile+' gr';
+		document.getElementById("bunsSel").value=huile+' gr';
+		document.getElementById("bunsLevure").value=huile+' gr';
+		let eau=Math.round(indexbuns*13089/10)/100;
+		document.getElementById("bunsEau").value=eau+' gr';
+		document.getElementById("bunsLait").value=eau+' gr';
+		let sucre=Math.round(indexbuns*2618/10)/100;
+		document.getElementById("bunsSucre").value=sucre+' gr';
+		let beurre=Math.round(indexbuns*5236/10)/100;
+		document.getElementById("bunsBeurre").value=beurre+' gr';
+		let puree=Math.round((poids-farine-3*huile-2*eau-sucre-beurre)*100)/100
+		document.getElementById("bunsPuree").value=puree+' gr';
 	});
 };
 claculSalaison();
